c858b07b6084fc5554b0e1f1e7249557f7296e31
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-01 15:52:39 +0200

    add info

---
+++ biglietto-treno/js/main.js
@@ -0,0 +1,8 @@
+// ^ RACCOLTA DATI
+const kmPrice = 0.21;
+const totalKm = prompt("quanti km vuoi percorrere");
+const passengerAge = prompt("quanti anni hai");
+
+console.log("kmPrice", kmPrice);
+console.log("totalKm", totalKm);
+console.log("passengerAge", passengerAge);
